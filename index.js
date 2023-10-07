fd2a9ae6671fbaecedbc0592844c482c8832e267
Pyrex141 <57811153+Pyrex141@users.noreply.github.com>
2023-10-07 16:55:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,220 @@
+const fetch = require("node-fetch")
+const fs = require('fs');
+const { Client, GatewayIntentBits } = require('discord.js');
+const ignoredChannelsFile = './ignoredChannels.json';
+const nicknamesFile = "./nicknames.json"; // Ajout du fichier de surnoms
 
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
+// Configuration
+const config = {
+    discordToken: "NzU3NjcxNDQ3Mzc1MTE4NDA3.X2jyhQ.aupC4O_nckQOsK9aTZvqx0ZuDOc", // token Discord
+    apiKey: "wr-MbekeRlC1azpUOiU2bqme2", // clé d'API OpenAI
+    memorySize: 10,
+  };
 
+// Création du client Discord
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
 
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+//Tableau "mémoire"
+let memory = Array(config.memorySize);
 
+// Gestionnaire d'événements "ready"
+client.once("ready", () => {
+    console.log("Nelly connectée!");
+  });
 
-const app = express();
-// app.use(bodyParser.json());
+// Gestionnaire d'événements "messageCreate"
+client.on("messageCreate", handleMessage);
 
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
-});
+// Connexion du bot Discord
+client.login(config.discordToken);
 
+// Fonction pour gérer les messages
+async function handleMessage(message) {
 
-
-
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
-
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
+    const userID = message.author.id;
+    const content = message.content;
+    const channelId = message.channel.id;
+    // Load the ignored channels
+    const ignoredChannels = loadIgnoredChannels();
+    // Charger le fichier de surnoms existant
+    const nicknames = loadNicknames(); 
+  
+    let   shouldGenerateResponse = '757671447375118407' == message.member.id.toString() ? false : Math.floor(Math.random() * 20) == 1
+    const isAskCommand = content.startsWith("+ask")
+    const isRebootCommand = content.startsWith("+reboot");
+  
+    // Vérifier si l'utilisateur existe dans le fichier nicknames.json
+    let userNickname = nicknames.userIDs[userID];
+    if (!userNickname) {
+        userNickname = message.member.user.username; // Utiliser le pseudo Discord par défaut
     }
 
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
+    // Gestion de la mémoire
+    addToMemory(userNickname + ': ' + (isAskCommand ? content.substring(4) : content));
+    consoleInfo();
+  
+    if (isAskCommand || (shouldGenerateResponse && ignoredChannels[channelId] !== true)) {
+      await message.channel.sendTyping();
+      try {
+        const response = await ask();
+        message.channel.send(response);
+      } catch (error) {
+        console.error("Erreur lors de la génération de la réponse :", error);
       }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
     }
+  
+    if (isRebootCommand) {
+      await message.react('👍');
+      clearMemory();
+    }
+    
+    if (content.startsWith("+getprompt")) {
+      const currentPrompt = getPrompt();
+      message.channel.send(`Le prompt actuel est : "${currentPrompt}"`);
+    }
+    
+    if (content.startsWith("+setprompt")) {
+      const newPrompt = content.substring(10);
+      setPrompt(newPrompt);
+      await message.react('👍');
+      clearMemory();
+    }
+
+    if (content.startsWith("+ignoreChannel")) {
+
+        ignoredChannels[channelId] = ignoredChannels[channelId] !== true;  
+        saveIgnoredChannels(ignoredChannels);
+        await message.react('👍');
+    }
+
+    if (content.startsWith("+nickname")) {
+        // Stocker le surnom de l'utilisateur dans le fichier JSON
+        const nickname = content.substring(9).trim();
+        nicknames.userIDs[userID] = nickname;
+        saveNicknames(nicknames);
+        await message.react('👍');
+      }
+    
   }
 
-});
-
-
-
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
+  // Fonction pour ajouter un message à la mémoire
+function addToMemory(message) {
+    memory.push(message);
+    if (memory.length > config.memorySize) {
+      memory.shift(); // Retirer le message le plus ancien s'il dépasse la taille limite
     }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
+}
+
+// Fonction pour afficher la mémoire dans la console
+function consoleInfo() {
+    console.clear();
+    console.log(memory.join("\n"));
   }
-})
 
+// Fonction pour réinitialiser la mémoire
+function clearMemory() {
+    memory = [];
+    consoleInfo();
+  }
 
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
+async function ask() {
 
+    const personality = getPrompt()
+    const prompt = memory + "\nNelly-chan: "
+        
+    try {
+        
+        const response = await fetch("https://thirdparty.webraft.in/v1/chat/completions", {
+        method: 'POST',
+        headers : {
+            "Authorization" : `Bearer ${config.apiKey}`,
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(
+            {
+            "model": "gpt-4",
+            "max_tokens": 100,
+            "messages": [
+                {
+                    "role" : "system",
+                    "content": `${personality}`
+                },
+                {
+                    "role" : "user",
+                    "content": `${prompt}`
+                }
+            ]
+        }) 
+    });
 
-app.listen(8999, () => {
+    const responseData = await response.json();
+    console.log(responseData);
 
-})
+    const resultContent = JSON.stringify(responseData.choices[0].message.content.split("\n")[0]).replace(/["]+/g, '');
 
+    return resultContent;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Fonction pour changer le prompt
+function setPrompt(newPrompt) {
+  fs.writeFileSync('prompt.txt', newPrompt);
+}
+
+// Fonction pour récupérer le prompt actuel
+function getPrompt() {
+  try {
+    return fs.readFileSync('prompt.txt', 'utf8');
+  } catch (error) {
+    console.log(error)
+    return 'Erreur lors de la lecture du fichier prompt.txt';
+  }
+}
+
+function loadIgnoredChannels() {
+    try {
+        const ignoredChannels = require(ignoredChannelsFile);
+        return ignoredChannels;
+    } catch (error) {
+        console.error("Error loading ignored channels:", error);
+        return {};
+    }
+}
+
+function saveIgnoredChannels(ignoredChannels) {
+    try {
+        fs.writeFileSync(ignoredChannelsFile, JSON.stringify(ignoredChannels, null, 2));
+    } catch (error) {
+        console.error("Error saving ignored channels:", error);
+    }
+}
+
+// Fonction pour charger les surnoms depuis le fichier JSON
+function loadNicknames() {
+    try {
+      const nicknamesData = fs.readFileSync(nicknamesFile, "utf8");
+      return JSON.parse(nicknamesData);
+    } catch (error) {
+      console.error("Error loading nicknames:", error);
+      return { userIDs: {} };
+    }
+  }
+  
+  // Fonction pour sauvegarder les surnoms dans le fichier JSON
+  function saveNicknames(nicknames) {
+    try {
+      fs.writeFileSync(nicknamesFile, JSON.stringify(nicknames, null, 2));
+    } catch (error) {
+      console.error("Error saving nicknames:", error);
+    }
+  }
